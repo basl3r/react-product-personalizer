d58a895c2352ec97ed1257f0c59430001c3ed41a
basl3r <ajgrociak@gmail.com>
2024-01-18 22:01:39 +0100

    Move image rendering functionality to ProductImage

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,6 +2,7 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import ProductImage from '../ProductImage/ProductImage';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import shortid from 'shortid';
@@ -47,12 +48,7 @@ const Product = props => {
 
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt={props.title}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
-      </div>
+      <ProductImage props={props} currentColor={currentColor} />
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
